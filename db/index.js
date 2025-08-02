9ccb18c40550ca44fa8a2822d116a00c84640cdc
Irshad-Mehsud <irshadahmad19059@gmail.com>
2025-08-02 21:03:20 +0500

    Adding dotenv to make it secure

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv/config";
 
-
-const url = `mongodb+srv://irshadahmad:irshadahmad@irshadcluster.w5dqwxs.mongodb.net/?retryWrites=true&w=majority&appName=IrshadCluster`;
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@irshadcluster.w5dqwxs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=IrshadCluster`;
 
 mongoose.connect(url);
 
